a400fda25d98897509b4eae7a432e3f547ed355d
ChrisWu1997 <rundi.wu@columbia.edu>
2021-07-08 08:57:12 -0400

    fix Lamp scripts

--- scripts/lamp/dec_lamp_seq2seq.sh
+++ scripts/lamp/dec_lamp_seq2seq.sh
@@ -3,5 +3,6 @@ python test.py --dec \
                 --data_root data \
                 --category Lamp \
                 --ckpt 1000 \
-                --format mesh \
-                --fake_z_path proj_log/pqnet-PartNet-Lamp/results/fake_z_ckpt80000_num100.h5
+                --by_part False \
+                --format voxel \
+                --fake_z_path proj_log/pqnet-PartNet-Lamp/results/fake_z_ckpt80000_num2000.h5
--- scripts/lamp/test_lamp_lgan.sh
+++ scripts/lamp/test_lamp_lgan.sh
@@ -2,5 +2,5 @@ python latentGAN.py --test \
                     --data_root proj_log/pqnet-PartNet-Lamp/results/enc-ckpt-1000/train \
                     --category Lamp \
                     --ckpt 80000 \
-                    --n_samples 100 \
+                    --n_samples 2000 \
                     -g 0
